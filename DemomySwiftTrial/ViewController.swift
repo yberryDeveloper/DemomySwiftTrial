81db768e9ddea14acf4c1e3397f16fe5bf157adc
Puneet <puneet.yberry@gmail.com>
2018-09-07 11:51:25 +0530

    Initial Commit

---
+++ DemomySwiftTrial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemomySwiftTrial
+//
+//  Created by Neelesh Jain on 9/7/18.
+//  Copyright © 2018 Neelesh Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
